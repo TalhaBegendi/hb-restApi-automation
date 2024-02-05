1e0022c6943b22a83e5fe58c7a870aead0a7e671
TalhaBegendi <67201575+TalhaBegendi@users.noreply.github.com>
2024-02-06 01:16:20 +0300

    Update LanguageStore.java

--- src/test/java/Store/LanguageStore.java
+++ src/test/java/Store/LanguageStore.java
@@ -4,6 +4,7 @@ import model.AuthorizationValue;
 import model.LanguageRequest;
 
 public class LanguageStore {
+    
     public LanguageRequest languageStore() {
         LanguageRequest languageRequest = new LanguageRequest();
         languageRequest.setSwaggerUrl("http://petstore.swagger.io/v2/swagger.json");
