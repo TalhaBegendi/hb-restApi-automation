a41d0cb2806a47f690816f877d7e4ddcd89727a9
TalhaBegendi <67201575+TalhaBegendi@users.noreply.github.com>
2024-02-07 17:33:27 +0300

    Update APIConstants.java

--- src/test/java/constant/APIConstants.java
+++ src/test/java/constant/APIConstants.java
@@ -1,10 +1,7 @@
 package constant;
 
-public final class APIConstants {
-
-    public static class SwaggerAPI {
+    public static class APIConstants {
         public static final String BASE_URL = "https://generator.swagger.io/";
         public static final String API_POST_SUFFIX_LANGUAGE = "api/gen/clients/ada";
         public static final String API_GET_SUFFIX_LANGUAGE = "api/gen/download/";
-    }
 }
