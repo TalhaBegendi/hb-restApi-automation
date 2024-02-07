1b15f4e5a1d010b5d9f2578e687ba73d0358040c
TalhaBegendi <67201575+TalhaBegendi@users.noreply.github.com>
2024-02-07 17:33:50 +0300

    Update RequestApiService.java

--- src/test/java/Request/RequestApiService.java
+++ src/test/java/Request/RequestApiService.java
@@ -12,14 +12,14 @@ public class RequestApiService extends ApiRequestHandler {
     LanguageStore languageStore = new LanguageStore();
 
     public RequestApiService() {
-        super(APIConstants.SwaggerAPI.BASE_URL);
+        super(APIConstants.BASE_URL);
     }
 
     public void postLanguage() throws UnsupportedEncodingException {
         Map<String, Object> header = new HashMap<String, Object>() {{
             put("Content-Type", "application/json");
         }};
-        Response response = postRequest(APIConstants.SwaggerAPI.API_POST_SUFFIX_LANGUAGE,header,languageStore.languageStore());
+        Response response = postRequest(APIConstants.API_POST_SUFFIX_LANGUAGE,header,languageStore.languageStore());
     }
 
     public void getLanguageFileDownload() {
@@ -27,6 +27,6 @@ public class RequestApiService extends ApiRequestHandler {
         Map<String, Object> header = new HashMap<String, Object>() {{
             put("Accept", "application/octet-stream");
         }};
-        Response response = getRequest(APIConstants.SwaggerAPI.API_GET_SUFFIX_LANGUAGE + fileCode,header);
+        Response response = getRequest(APIConstants.API_GET_SUFFIX_LANGUAGE + fileCode,header);
     }
 }
